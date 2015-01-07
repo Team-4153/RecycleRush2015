e981803919ea22f83d27cc3ca1f45f9067440615
4153student <4153student@Team4153-PC>
2015-01-06 17:58:53 -0700

    Running a single talon, and includes some commented out code to reset the sticky faults.

--- src/org/usfirst/frc/team4153/robot/Robot.java
+++ src/org/usfirst/frc/team4153/robot/Robot.java
@@ -1,7 +1,11 @@
 
 package org.usfirst.frc.team4153.robot;
 
+import java.nio.IntBuffer;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.hal.PDPJNI;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -11,26 +15,42 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class Robot extends IterativeRobot {
+	
+	Talon talon1;
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-    		//test comment
+    	
+    	// This line when deployed seems to get rid of the sticky fault
+    	// but prevents robot code from happening
+    	//PDPJNI.clearPDPStickyFaults(IntBuffer.wrap(new int[]{0}));
+
+    	talon1 = new Talon(1);
+    	try {
+    		// CANJaguar testJag = new CANJaguar(1);
+    	}catch (Exception e){
+    		// This will throw an error because there is no can
+    		
+    	}
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
+    	//CANJaguar jag1 = new CANJaguar(1);
+    	
+    	talon1.set(0.25);
     }
+    
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        
+    	talon1.set(0.);
     }
     
     /**
